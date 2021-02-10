fa46f50e78416dda27a0c57e26572b46decb3ab8
Steffen Exler <Steffen.Exler@pm.me>
2021-02-10 18:32:54 +0100

    add test for market

--- django_crypto_trading_bot/trading_bot/tests/models/test_market.py
+++ django_crypto_trading_bot/trading_bot/tests/models/test_market.py
@@ -11,6 +11,10 @@ from ..factories import AccountFactory, MarketFactory
 
 @pytest.mark.django_db()
 class TestMarket(unittest.TestCase):
+    def test_to_string(self):
+        market: Market = MarketFactory()
+        assert market.__str__() == "TRX/BNB"
+
     def test_symbol(self):
         # check if symbol create the right symbol
         market: Market = MarketFactory()
@@ -23,6 +27,40 @@ class TestMarket(unittest.TestCase):
 
         market_exchange = client.market(market.symbol)
         assert market.symbol == market_exchange["symbol"]
+
+    def test_market_id(self):
+        market: Market = MarketFactory()
+        assert market.market_id == "trxbnb"
+
+    def test_baseId(self):
+        market: Market = MarketFactory()
+        assert market.baseId == "trx"
+
+    def test_quoteId(self):
+        market: Market = MarketFactory()
+        assert market.quoteId == "bnb"
+
+    def test_get_min_max_price(self):
+        market: Market = MarketFactory()
+        getcontext().prec = market.precision_amount
+
+        # test minimum
+        assert "{:.3f}".format(
+            market.get_min_max_price(market.limits_price_min / 2)
+        ) == "{:.3f}".format(0.100)
+        # test maximum
+        assert "{:.3f}".format(
+            market.get_min_max_price(market.limits_price_max * 2)
+        ) == "{:.3f}".format(market.limits_price_max)
+        # test valid price
+        assert "{:.3f}".format(
+            market.get_min_max_price(Decimal(10.3))
+        ) == "{:.3f}".format(10.3)
+        # test precision_price 0
+        market.precision_price = 0
+        assert "{:.3f}".format(
+            market.get_min_max_price(Decimal(10.321))
+        ) == "{:.3f}".format(10.321)
 
     def test_get_min_max_order_amount(self):
         market: Market = MarketFactory()
